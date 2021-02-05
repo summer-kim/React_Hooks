622f0ad7899760ef32c6c92194d62742c8ffccef
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-05 17:02:18 +0900

    initial commit and basic useEffect.js

---
+++ src/UseEffect.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const UseEffect = () => {
+  const sayHello = () => {
+    console.log('Hello');
+  };
+  const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  useEffect(() => {
+    sayHello();
+  });
+  //useEffect with empty dependency
+  //works as componentDidMount, componentWillUnmount, componentDidUpdate which is excuted when you click button
+
+  //useEffect with dependency(non-empty second parameter)
+  //works as componentDidMount, componentWillUpdate
+  return (
+    <div>
+      <button onClick={() => setNum(num + 1)}>{num}</button>
+      <button onClick={() => setNum2(num2 + 1)}>{num2}</button>
+    </div>
+  );
+};
+
+export default UseEffect;
