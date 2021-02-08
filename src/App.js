0a8739dbb2a1c912b26aaa756c34b2a3e97e4a2a
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-08 19:51:14 +0900

    rename hook folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import UseEffectCompo from './utils/UseEffectCompo';
-import UsePutInCompo from './utils/UsePutInCompo';
-import UseTabCompo from './utils/UseTabCompo';
-import UseClickCompo from './utils/UseClickCompo';
-import UseConfirmCompo from './utils/UseConfirmCompo';
-import UseProtectCompo from './utils/UseProtectCompo';
-import UseBeforeLeaveCompo from './utils/UseBeforeLeaveCompo';
-import UseScrollCompo from './utils/UseScrollCompo';
-import UseFadeinCompo from './utils/UseFadeinCompo';
-import UseOnlineCompo from './utils/UseOnlineCompo';
+import UseEffectCompo from './Hooks/UseEffectCompo';
+import UsePutInCompo from './Hooks/UsePutInCompo';
+import UseTabCompo from './Hooks/UseTabCompo';
+import UseClickCompo from './Hooks/UseClickCompo';
+import UseConfirmCompo from './Hooks/UseConfirmCompo';
+import UseProtectCompo from './Hooks/UseProtectCompo';
+import UseBeforeLeaveCompo from './Hooks/UseBeforeLeaveCompo';
+import UseScrollCompo from './Hooks/UseScrollCompo';
+import UseFadeinCompo from './Hooks/UseFadeinCompo';
+import UseOnlineCompo from './Hooks/UseOnlineCompo';
 
 function App() {
   return (
